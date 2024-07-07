a35890867a8a1768c4698a48645e66350e94706c
Sunscar-Sony <166742286+Sunscar-Sony@users.noreply.github.com>
2024-07-07 23:39:35 +0530

    Update CartScreen.js

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import NavBar from '../components/NavBar'
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import NavBar from "../components/NavBar";
 
 export default function CartScreen() {
   return (
-    <View>
-      <Text>CartScreen</Text>
+    <SafeAreaView style={{flex:1}}>
+      <View style={{flex:1}}>
+      <Text style={{justifyContent:'center', alignItems:'center', textAlign:'center', marginTop:100}}>Cart Screen</Text>
+      </View>
       <NavBar />
-    </View>
-  )
+    </SafeAreaView>
+  );
 }

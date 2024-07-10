2ea83b7fd117215c3ea2ba4d88bd0aede918b76b
Sunscar-Sony <166742286+Sunscar-Sony@users.noreply.github.com>
2024-07-10 16:51:45 +0530

    images updated

--- styles/CardVendor.js
+++ styles/CardVendor.js
@@ -4,7 +4,7 @@ const styles = StyleSheet.create({
   card: {
     width: "100%",
     backgroundColor: "#fff",
-    padding: 20,
+    padding: 10,
     marginVertical: 10,
     borderRadius: 10,
     shadowColor: "#000",
@@ -15,18 +15,20 @@ const styles = StyleSheet.create({
     alignItems: "left",
   },
   image: {
-    width: "90%",
+    width: "100%",
     alignSelf: "center",
-    height: 100,
-    borderRadius: 10,
+    height: 120,
+    borderRadius: 7,
     marginBottom: 10,
   },
   title: {
+    marginLeft: 10,
     textAlign: "left",
     fontSize: 22,
     fontWeight: "bold",
   },
   rating: {
+    marginLeft: 10,
     fontSize: 16,
     color: "#888",
   },

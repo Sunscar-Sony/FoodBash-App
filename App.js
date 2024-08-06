a6ba2164d1beca75ed50045f6f005e1bb1690b27
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-08-06 10:21:57 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Navigation from './navigation';
 
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <Navigation />
-    </SafeAreaProvider>
-  );
-}

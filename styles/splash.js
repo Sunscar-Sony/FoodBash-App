9d9c7563baf64171932ab869abba8b12e9038327
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-08-11 01:52:53 +0530

    Update splash.js

--- styles/splash.js
+++ styles/splash.js
@@ -1,1 +1,48 @@
+import { StyleSheet, Dimensions } from 'react-native';
 
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: width,
+    height: height,
+    resizeMode: 'cover',
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+  },
+  mainHeading: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 20,
+  },
+  subHeading: {
+    fontSize: 16,
+    color: '#5D5D5D',
+    marginBottom: 50,
+  },
+  loader: {
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  footerText: {
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: 20,
+    textAlign: 'center',
+    color: '#000',
+    fontSize: 12,
+  },
+});
+
+export default styles;

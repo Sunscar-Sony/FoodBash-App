01efe9ecda521aff6860d35980a7307a0d920454
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-08-13 11:44:02 +0530

    Update VendorImage.js

--- data/VendorImage.js
+++ data/VendorImage.js
@@ -1,9 +1,1 @@
-const images = {
-    "vendor1.png": require("../assets/vendors/vendor1.png"),
-    "vendor2.png": require("../assets/vendors/vendor2.png"),
-    "vendor3.png": require("../assets/vendors/vendor3.png"),
-    "vendor4.png": require("../assets/vendors/vendor4.png"),
-  };
-  
-  export default images;
-  
+

1186ea207b01ac123ed13339f8f89676673bc7f1
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-08-22 10:40:45 +0530

    Update OrderCard.js

--- components/OrderCard.js
+++ components/OrderCard.js
@@ -1,1 +1,48 @@
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import styles from "../styles/CardOrder";
 
+const OrderCard = ({ item, addToCart }) => {
+  const [addedToCart, setAddedToCart] = useState(false);
+
+  const handleAddToCart = () => {
+    if (addedToCart) {
+      // Remove from cart functionality
+      // Write you logic
+      setAddedToCart(false);
+    } else {
+      // Add to cart functionality
+      addToCart(item);
+      setAddedToCart(true);
+    }
+  };
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: item.image }} style={styles.image} />
+      </View>
+      <View style={styles.info}>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.subheading}>{item.subheading}</Text>
+        <Text style={styles.price}>${item.price}</Text>
+      </View>
+      <TouchableOpacity
+        style={[
+          styles.addToCartButton,
+          addedToCart ? styles.addedToCartButton : null,
+        ]}
+        onPress={handleAddToCart}
+      >
+        {addedToCart ? (
+          <Ionicons name="checkmark" size={24} color="#000" />
+        ) : (
+          <Ionicons name="cart-outline" size={24} color="#000"/>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default OrderCard;
